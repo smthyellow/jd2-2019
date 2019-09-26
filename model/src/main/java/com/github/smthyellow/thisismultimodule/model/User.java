5160c6fbafb893c3fe6fd6d43edde62c42613b75
smthyellow <nickol.husky@mail.com>
2019-09-27 02:22:04 +0300

    Sixth commit

--- model/src/main/java/com/github/smthyellow/thisismultimodule/model/User.java
+++ model/src/main/java/com/github/smthyellow/thisismultimodule/model/User.java
@@ -3,4 +3,12 @@ package com.github.smthyellow.thisismultimodule.model;
 public class User {
     private String name;
     private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
